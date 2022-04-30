0b9a19cf160a89d6994deff2ad4e21c32ed9d879
jesther-ai <jesthersilvestre@yahoo.com>
2022-04-30 12:53:39 +0800

    added search box

---
+++ lib/widgets/main_card.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MainCard extends StatelessWidget {
+  const MainCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      height: 150,
+      width: double.infinity,
+    );
+  }
+}
